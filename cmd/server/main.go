91b23c37f9e0b38e61d4860828bf92158f74cc68
jack874-bit <jack.harper874@gmail.com>
2026-05-22 13:07:29 +0100

    first publish

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	newConnection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Unable to create new connection")
+	}
+	defer newConnection.Close()
+	fmt.Println("Connection was successful")
+
+	newChan, err := newConnection.Channel()
+	if err != nil {
+		log.Fatalf("Unable to create new channel")
+	}
+	err = pubsub.PublishJSON(newChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatalf("Unable to publish: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down...")
 }
